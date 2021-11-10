081bd2b80e506777dddc807895188db734d195f5
Giovanni Malcolm <malcolm.giovanni@gmail.com>
2021-11-10 02:13:46 -0500

    user routes complete. doing user model next. will continue with controllers/models development

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const {Post} = require('../models/')
+
+router
+    .get('/', async (req, res) => {
+
+
+    }
+
+
+
+
+module.exports = router;
+
+
+router
+    .get('/', async (req, res) => {
+
+
+    }
+
+
+    router
+    .get('/dashboard', async (req, res) => {
+
+
+    }
+
+
+
+
